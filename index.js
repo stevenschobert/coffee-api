076481fc318065756fd782bfa0357d5e0ce850e2
Steven Schobert <spschobert@gmail.com>
2014-02-25 16:27:25 -0600

    add slack webhook

--- index.js
+++ index.js
@@ -26,6 +26,29 @@ app.get('/api/v1/measurements/:cups?', function (req, res) {
   });
 });
 
+// support slack requests
+app.get('/webhooks/slack', function (req, res) {
+  coffeeBear(function (err, data) {
+    var cups = req.query.text;
+
+    if (err) {
+      res.send('Sorry, something went wrong processing your request');
+    }
+
+    if (cups === undefined) {
+      res.send('How many cups did you want to make? eg: /coffee 5');
+      return;
+    }
+
+    if (data[cups] !== undefined) {
+      res.send(JSON.stringify(data[cups]));
+      return;
+    }
+
+    res.send("Sorry, I don't know how to make that much coffee.");
+  });
+});
+
 app.listen(app.get('port'), function () {
   console.log('listening on port ' + app.get('port'));
 });
